67c0d91cdf6ec332f9c349291b6c16f6d6be8029
Ailtonjr <ailtoncardoso0@gmail.com>
2016-07-13 16:28:08 -0300

    Finalizado algoritmo para Busca Iterada Task #7 - Busca Local Iterada

--- BLM e BLI/src/br/univali/BLM_BLI/controle/ControladorBLIterada.java
+++ BLM e BLI/src/br/univali/BLM_BLI/controle/ControladorBLIterada.java
@@ -9,11 +9,17 @@ public class ControladorBLIterada {
     public ControladorBLIterada() {
         bli = new BLIterada();
         relatorio = "";
+        //System.out.println("\n\n-------------------\n1º Maquinario");
         bli.novoMaquinario(10, 32);     //  10^1.5 = 31,6227
+        //System.out.println("\n\n-------------------\n2º Maquinario");
         bli.novoMaquinario(10, 100);    //  10^2.0 = 100
+        //System.out.println("\n\n-------------------\n3º Maquinario");
         bli.novoMaquinario(20, 89);     //  20^1.5 = 89,4427
+        //System.out.println("\n\n-------------------\n4º Maquinario");
         bli.novoMaquinario(20, 400);    //  20^2.0 = 400
+        //System.out.println("\n\n-------------------\n5º Maquinario");
         bli.novoMaquinario(50, 354);    //  50^1.5 = 353,5533
+        //System.out.println("\n\n-------------------\n6º Maquinario");
         bli.novoMaquinario(50, 2500);   //  50^2.0 = 2500
     }
     
